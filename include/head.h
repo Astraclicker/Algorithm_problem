e0e052120ec1fd3a9b18774491e36dbb41fa65df
Astraclicker <2867314230@qq.com>
2026-06-10 19:17:49 +0800

    头文件添加二叉树支持

--- include/head.h
+++ include/head.h
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//链表
 struct ListNode {
     int val;
     ListNode *next;
@@ -14,6 +15,22 @@ struct ListNode {
     }
 
     ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
+//二叉树
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
     }
 };
 
